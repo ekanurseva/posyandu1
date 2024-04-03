915fd8a984fb89155d907eccbf141e600a30e174
Eka Nurseva Saniyah <ekanursevas@gmail.com>
2024-04-03 12:01:12 +0700

    feat: grafik line berat badan

--- ortu/index.php
+++ ortu/index.php
@@ -1,3 +1,38 @@
+<?php
+require_once '../controller/penimbanganController.php';
+
+$ibu = cari_user();
+$id_ibu = $ibu['id_user'];
+$jadwal = [];
+
+$balita = query("SELECT * FROM balita WHERE id_user = $id_ibu");
+
+foreach ($balita as $b) {
+    $id_balita = $b['id_balita'];
+    $penimbangan = query("SELECT * FROM penimbangan WHERE id_balita = $id_balita");
+
+    foreach ($penimbangan as $pen) {
+        $bb[] = $pen['berat_badan'];
+        $id_jadwal = $pen['idjadwal'];
+
+        $cek_jadwal = query("SELECT * FROM jdw_posyandu WHERE idjadwal = $id_jadwal")[0];
+
+        if (array_search(cari_tanggal($cek_jadwal['jadwal'], 'd F Y'), $jadwal) != false) {
+            $jadwal[] = cari_tanggal($cek_jadwal['jadwal'], 'd F Y');
+        }
+    }
+
+    $berat[] = $bb;
+    $nama_balita[][] = $b['nama_balita'];
+
+    // var_dump($berat);
+    // var_dump($jdw);
+    // var_dump($nama_balita);
+}
+
+var_dump($jadwal);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -14,6 +49,8 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css" />
     <link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../style.css">
+    <!-- Include Chart.js -->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
     <title>Posyandu</title>
 
@@ -42,9 +79,9 @@
             <!-- sidebar selesai -->
 
             <div class="contents">
-                <h2 class="text-center fw-bold" style="color: black;">WELCOME TO</h2>
-                <div class="d-flex justify-content-center">
-                    <img src="../image/img5.png" alt="Posyandu">
+                <h2 class="text-center fw-bold" style="color: black;">WELCOME TO POSYANDU</h2>
+                <div class="mt-3">
+                    <canvas id="myChart" style="border: solid 1px; padding: 10px;"></canvas>
                 </div>
 
                 <!-- about -->
@@ -91,6 +128,48 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
         integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous">
         </script>
+    <script>
+        // Ambil referensi elemen canvas
+        var ctx = document.getElementById('myChart').getContext('2d');
+
+        // Data balita dan jadwal penimbangan
+        var dataBalita = {
+            labels: <?php echo json_encode($jadwal); ?>, // Usia balita (dalam bulan)
+            datasets: [{
+                label: 'Berat Badan ' + "<?php echo $balita[0]['nama_balita'] ?>", // Label grafik
+                data: <?php echo json_encode($bb); ?>, // Berat badan balita (dalam kg)
+                fill: false, // Tidak mengisi area di bawah garis
+                borderColor: 'rgb(80, 197, 252)', // Warna garis
+                tension: 0.1 // Tegangan kurva
+            }]
+        };
+
+        // Opsi grafik
+        var options = {
+            scales: {
+                x: {
+                    title: {
+                        display: true,
+                        text: 'Usia Balita dan Jadwal Penimbangan' // Label sumbu x
+                    }
+                },
+                y: {
+                    title: {
+                        display: true,
+                        text: 'Berat Badan (kg)' // Label sumbu y
+                    }
+                }
+            }
+        };
+
+        // Buat grafik line
+        var myChart = new Chart(ctx, {
+            type: 'line',
+            data: dataBalita,
+            options: options
+        });
+
+    </script>
 </body>
 
 </html>
